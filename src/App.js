c4f7a39f8d5fc773ffce994edde5f7c5d8f0a780
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-02-22 23:27:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Report from "./pages/Report";
 import SOS from "./pages/SOS";
 import Call from "./pages/Call";
 import Hub from "./pages/Hub";
+import "./styles/global.css";
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="/sos" element={<SOS />} />
-        <Route path="/call" element={<Call />} />
-        <Route path="/hub" element={<Hub />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/report" element={<Report />} />
+                    <Route path="/sos" element={<SOS />} />
+                    <Route path="/call" element={<Call />} />
+                    <Route path="/hub" element={<Hub />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
+};
 
 export default App;
-  
