75e1357a2459f92d4688f9ca60123d3b74e6b0e9
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-02-22 23:09:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import SOS from "./pages/SOS";
+import Call from "./pages/Call";
+import Hub from "./pages/Hub";
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/sos" element={<SOS />} />
+        <Route path="/call" element={<Call />} />
+        <Route path="/hub" element={<Hub />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
+  
