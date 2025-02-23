f9de47b9fbacd7eff91bc158a4116a8145b2744d
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-02-23 09:16:30 +0100

    Create ComingSoon.js

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon">
+      <img src="/images/cone.svg" alt="Coming Soon" className="coming-soon-image" />
+      <h2>Coming soon</h2>
+      <p>PreFem is a web app designed by feministe to combat femicide in Nigeria.</p>
+    </div>
+  );
+};
+
+export default ComingSoon;
