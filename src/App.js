10a8e42d2fe3033e8e907b31d317f4c8af5d9114
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-02-23 09:14:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Report from "./pages/Report";
 import SOS from "./pages/SOS";
 import Call from "./pages/Call";
 import Hub from "./pages/Hub";
+import Chat from "./pages/Chat";
 import "./styles/global.css";
 
 const App = () => {
-    return (
-        <Router>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/report" element={<Report />} />
-                    <Route path="/sos" element={<SOS />} />
-                    <Route path="/call" element={<Call />} />
-                    <Route path="/hub" element={<Hub />} />
-                </Routes>
-            </main>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <main style={{ paddingBottom: "80px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/sos" element={<SOS />} />
+          <Route path="/call" element={<Call />} />
+          <Route path="/hub" element={<Hub />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
